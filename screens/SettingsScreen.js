97c082a15a8c6f578be9ca4f036a64f9026748e2
unknown <v.auricchio@web.de>
2020-04-01 15:44:26 +0200

    Adjust bottom navigation

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function SettingsScreen() {
+  return (
+    <View style={styles.container} contentContainerStyle={styles.contentContainer}>
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  contentContainer: {
+    paddingTop: 15,
+  }
+});
---
+++ screens/WeatherScreen.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import {  StyleSheet, Text, View } from 'react-native';
+
+export default function WeatherScreen() {
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+WeatherScreen.navigationOptions = {
+  header: null,
+};
+
+function DevelopmentModeNotice() {
+  if (__DEV__) {
+    const learnMoreButton = (
+      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
+        Learn more
+      </Text>
+    );
+
+    return (
+      <Text style={styles.developmentModeText}>
+        Development mode is enabled: your app will be slower but you can use useful development
+        tools. {learnMoreButton}
+      </Text>
+    );
+  } else {
+    return (
+      <Text style={styles.developmentModeText}>
+        You are not in development mode: your app will run at full speed.
+      </Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  developmentModeText: {
+    marginBottom: 20,
+    color: 'rgba(0,0,0,0.4)',
+    fontSize: 14,
+    lineHeight: 19,
+    textAlign: 'center',
+  },
+  contentContainer: {
+    paddingTop: 30,
+  },
+});
